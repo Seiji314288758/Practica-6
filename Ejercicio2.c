2479c520e1f185aeda956d11507adcf46153e76f
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 13:59:55 -0600

    Create Ejercicio2.c

---
+++ Ejercicio2.c
@@ -0,0 +1,15 @@
+/*Victor Seiji Nieto Morita
+practica 6 ejercicio 2*/
+
+#include <stdio.h>
+int main() 
+{
+short enteroNumero1 = 115;
+signed int enteroNumero2 = 55;
+unsigned long enteroNumero3 = 789;
+char caracterA = 65;
+char caracterB = 'B';
+float puntoFlotanteNumero1 = 89.8;
+unsigned double puntoFlotanteNumero2 = 238.2236;
+return 0;
+}
