668942060f016081a8451bd7ec386a64e8c89042
Seiji314288758 <nietoseiji@gmail.com>
2025-10-31 14:01:57 -0600

    Create Ejercicio4.c

---
+++ Ejercicio4.c
@@ -0,0 +1,18 @@
+/*Victor Seiji Nieto Morita
+practica 6 ejercicio 4*/
+
+#include <stdio.h>
+int main()
+{
+int entero;
+float flotante;
+printf("Ingresa el valor entero: ");
+scanf("%i", &entero);
+printf("El valor ingresado es: %d\n"
+printf("Ingresa el valor float: ");
+scanf("%f", &flotante);
+printf("El valor ingresado es: %f\n"
+return 0;
+, entero);
+, flotante);
+}
